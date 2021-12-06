9f6498151a213e40a95c64817ed588db2b428961
reinis dreska <rdreska@gmail.com>
2021-12-06 13:11:47 +0200

    modelis un dabun cordinatus nokliksinot

---
+++ model.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+    
+db =SQLAlchemy()
+    
+class place_model(db.Model):
+    __tablename__ = "table"
+
+    id = db.Column(db.Integer, primary_key=True)
+    place_id = db.Column(db.Integer(),unique = True)
+    name = db.Column(db.String())
+    description = db.Column(db.Text())
+    latitude = db.Column(db.Float())
+    longitude = db.Column(db.Float())
+
+    def __init__(self, place_id, name, description, latitude, longitude):
+        self.place_id = place_id
+        self.name = name
+        self.description = description
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"{self.name}"
